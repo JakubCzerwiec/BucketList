d7c4af5821e59c2340c5be2824cdd6240f58d2f1
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-12-06 19:57:01 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jakub Czerwiec  on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
